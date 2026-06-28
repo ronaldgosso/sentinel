0fa2d725131c25c03fd8195b3b892d768c33f289
Ronald Gosso <ronald.gosso@neptuneprolabs.com>
2026-06-28 11:29:40 +0300

    feat: export frontend vulnerabilities detector in init
    Add detect_frontend_vulnerabilities to the module exports.

--- src/sentinel/scanners/sast/detectors/__init__.py
+++ src/sentinel/scanners/sast/detectors/__init__.py
@@ -3,6 +3,7 @@ from .xss import detect_xss
 from .command_injection import detect_command_injection
 from .secrets import detect_hardcoded_secrets
 from .crypto import detect_insecure_crypto
+from .frontend import detect_frontend_vulnerabilities
 
 __all__ = [
     "detect_sql_injection",
@@ -10,4 +11,5 @@ __all__ = [
     "detect_command_injection",
     "detect_hardcoded_secrets",
     "detect_insecure_crypto",
+    "detect_frontend_vulnerabilities",
 ]
